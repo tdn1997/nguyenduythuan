5d9b8af1c737d58baba9955e3e2d105b3d86b0a0
Thuan <nauhtyud@gmail.com>
2025-04-23 21:39:11 +0700

    fix: ios:app_name

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home: NextPage<IProps> = (props: IProps) => {
         <title>Nguyen Duy Thuan</title>
         <meta property="al:ios:url" content="https://uonearth.urbn8.io/thuannguyen-c6ci?promoName=Thuan_Test_1_4_uon&channel=facebook" />
         <meta property="al:ios:app_store_id" content="6470965614" />
-        <meta property="al:ios:app_name" content="Uon.Earth" />
+        <meta property="al:ios:app_name" content="UonEarth_staging" />
 
         {/* <meta property="al:android:url" content="uonearth://" />
         <meta property="al:android:package" content="uon.earth" />
